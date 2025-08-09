a0e4f3ea96a9dbd2f5a5546ccb8bcad613380e1a
호현 <84440975+LockFix@users.noreply.github.com>
2025-08-09 20:30:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int memory[12];
+
+int dp(int x) {
+    if(x == 1) return 1;
+    if(x == 2) return 2;
+    if(x == 3) return 4;
+    if(memory[x] != 0) return memory[x];
+    return memory[x] = (dp(x - 1) + dp(x - 2) + dp(x - 3)) % 10007;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T; cin >> T;
+    while(T--){
+        int n; cin >> n;
+        cout << dp(n) << '\n';
+    }
+}   
