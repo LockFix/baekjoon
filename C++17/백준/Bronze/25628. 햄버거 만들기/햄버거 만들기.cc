6c0df196ad04be05d45695fd746011d2f51b8991
호현 <84440975+LockFix@users.noreply.github.com>
2025-08-20 22:24:13 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int A, B; cin >> A >> B;
+    if(B * 2 > A) cout << A / 2;
+    else cout << B;
+}   
