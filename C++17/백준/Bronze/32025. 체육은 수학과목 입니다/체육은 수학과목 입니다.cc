017581cb40163f55278e12fb6ee9b3edf291963d
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-06 01:12:59 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int H, W; cin >> H >> W;
+    cout << min(H, W) * 50;
+}   
