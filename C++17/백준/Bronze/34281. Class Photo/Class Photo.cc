20bb0861189dba39ab6678a45e9a27423eab3c87
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-11 01:33:10 +0900

    [Bronze V] Title: Class Photo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34281. Class Photo/Class Photo.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.1415926
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int w, l; cin >> w >> l;
+    cout << w * l;
+}  
