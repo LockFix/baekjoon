420a100b09ebddbade01cb5add633bedf3d31451
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-20 01:14:01 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int M, tmp, C; cin >> M >> tmp >> C;
+    cout << M * C;
+}
