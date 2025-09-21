e2a99bc4601b5be05d69ab6261e09eafaff867b8
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-21 20:59:47 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    short m, n, t; cin >> m >> n >> t;
+    cout << m * 2;
+}
