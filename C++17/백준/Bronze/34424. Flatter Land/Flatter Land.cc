6fcef093157d06698a60e0039b4075cff4aa9d6f
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-22 23:04:52 +0900

    [Bronze V] Title: Flatter Land, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34424. Flatter Land/Flatter Land.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    short n, x; cin >> n >> x;
+    cout <<  (n - 1) * x;
+}
