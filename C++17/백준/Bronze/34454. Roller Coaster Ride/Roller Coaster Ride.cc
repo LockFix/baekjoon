2fb80436261ae97a5a031a6f54495fa580918c82
호현 <84440975+LockFix@users.noreply.github.com>
2025-09-25 19:08:18 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, C, P; cin >> N >> C >> P;
+    if(C * P >= N) cout << "yes";
+    else cout << "no";
+}
