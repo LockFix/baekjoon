9170553585e479876a3fcb224f209576c48a5ac0
호현 <84440975+LockFix@users.noreply.github.com>
2025-11-09 16:47:58 +0900

    [Silver II] Title: 다각형그리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- C++17/백준/Silver/2641. 다각형그리기/다각형그리기.cc
+++ C++17/백준/Silver/2641. 다각형그리기/다각형그리기.cc
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+bool check(list<short> l, short arr[], short N, list<list<short>> *ans) {
+    for(int i = 0; i < N; i++) {
+        short index = 0;
+        for(int j : l) {
+            if(j != arr[index]) {
+                break;
+            }
+            index++;
+        }
+        if(index == N) {
+            ans->push_back(list<short>(arr, arr + N));
+            return true;
+        }
+        l.push_back(l.front());
+        l.pop_front();
+    }
+    return false;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -21,74 +40,16 @@ int main() {
     short M; cin >> M;
     while(M--) {
         short arr[N];
-        bool flag = true;
         for(int i = 0; i < N; i++) cin >> arr[i];
-        for(int i = 0; i < N; i++) {
-            short index = 0;
-            for(int j : l) {
-                if(j != arr[index]) {
-                    break;
-                }
-                index++;
-            }
-            if(index == N) {
-                ans.push_back(list<short>(arr, arr + N));
-                flag = false;
-                break;
-            }
-            l.push_back(l.front());
-            l.pop_front();
-        }
-        if(!flag) continue;
-        for(int i = 0; i < N; i++) {
-            short index = 0;
-            for(int j : l2) {
-                if(j != arr[index]) {
-                    break;
-                }
-                index++;
-            }
-            if(index == N) {
-                ans.push_back(list<short>(arr, arr + N));
-                flag = false;
-                break;
-            }
-            l2.push_back(l2.front());
-            l2.pop_front();
-        }
-        if(!flag) continue;
-        for(int i = 0; i < N; i++) {
-            short index = N - 1;
-            for(int j : l) {
-                if(j != arr[index]) {
-                    break;
-                }
-                index--;
-            }
-            if(index == -1) {
-                ans.push_back(list<short>(arr, arr + N));
-                flag = false;
-                break;
-            }
-            l.push_back(l.front());
-            l.pop_front();
-        }
-        if(!flag) continue;
-        for(int i = 0; i < N; i++) {
-            short index = N - 1;
-            for(int j : l2) {
-                if(j != arr[index]) {
-                    break;
-                }
-                index--;
-            }
-            if(index == -1) {
-                ans.push_back(list<short>(arr, arr + N));
-                break;
-            }
-            l2.push_back(l2.front());
-            l2.pop_front();
-        }
+        bool flag = check(l, arr, N, &ans);
+        if(flag) continue;
+        flag = check(l2, arr, N, &ans);
+        if(flag) continue;
+        l.reverse();
+        flag = check(l, arr, N, &ans);
+        if(flag) continue;
+        l2.reverse();
+        check(l2, arr, N, &ans);
     }
     cout << ans.size() << '\n';
     for(list<short> i : ans) {
