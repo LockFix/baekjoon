1e7c731e1175a5df6c262707083549332a8b50fa
호현 <84440975+LockFix@users.noreply.github.com>
2025-11-13 17:19:55 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 8 ms, Memory: 3072 KB -BaekjoonHub

---
+++ C++17/백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N, M; cin >> N >> M;
+    queue<int> q;
+    q.push(N);
+    int cnt[200000];
+    for(int i = 0; i < 200000; i++) cnt[i] = INT32_MAX;
+    cnt[N] = 0;
+    while(!q.empty()) {
+        int temp = q.front();
+        temp = q.front();
+        if(temp < M && cnt[temp * 2] > cnt[temp] + 1) {
+            q.push(temp * 2);
+            cnt[temp * 2] = cnt[temp] + 1;
+        }
+        if(temp + 1 <= M && cnt[temp + 1] > cnt[temp] + 1) {
+            q.push(temp + 1);
+            cnt[temp + 1] = cnt[temp] + 1;
+        }
+        if(temp - 1 >= 0 && cnt[temp - 1] > cnt[temp] + 1) {
+            q.push(temp - 1);
+            cnt[temp - 1] = cnt[temp] + 1;
+        }
+        q.pop();
+    }
+    int ans[cnt[M] + 1];
+    for(int i = M, tmp = cnt[M]; tmp >= 0; tmp--) {
+        ans[tmp] = i;
+        if(i % 2 == 0 && cnt[i / 2] + 1 == cnt[i]) {
+            i /= 2;
+            continue;
+        }
+        if(cnt[i + 1] + 1 == cnt[i]) {
+            i++;
+            continue;
+        }
+        if(cnt[i - 1] + 1 == cnt[i]) {
+            i--;
+            continue;
+        }
+    }
+    cout << cnt[M] << '\n';
+    for(int i : ans) cout << i << ' ';
+}
