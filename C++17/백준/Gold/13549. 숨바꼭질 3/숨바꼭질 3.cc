46e2d1dea88de595e02c29a74224d33a35be3ae3
호현 <84440975+LockFix@users.noreply.github.com>
2025-11-13 16:52:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2944 KB -BaekjoonHub

--- C++17/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ C++17/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -11,32 +11,26 @@ int main() {
     int N, M; cin >> N >> M;
     queue<int> q;
     q.push(N);
-    bool visited[200000];
     int cnt[200000];
-    for(int i = 0; i < 200000; i++) cnt[i] = -1;
+    for(int i = 0; i < 200000; i++) cnt[i] = INT32_MAX;
     cnt[N] = 0;
-    visited[N] = true;
     while(!q.empty()) {
         int temp = q.front();
         int size = q.size();
-        while(size-- && temp != M) {
-            temp = q.front();
-            if(temp < M && !visited[temp * 2]) {
-                q.push(temp * 2);
-                cnt[temp * 2] = min((cnt[temp * 2] == -1 ? INT32_MAX : cnt[temp * 2]), cnt[temp]);
-            }
-            if(temp + 1 <= M && !visited[temp + 1]) {
-                q.push(temp + 1);
-                cnt[temp + 1] = min((cnt[temp + 1] == -1 ? INT32_MAX : cnt[temp + 1]), cnt[temp] + 1);
-            }
-            if(temp - 1 >= 0 && !visited[temp - 1]) {
-                q.push(temp - 1);
-                cnt[temp - 1] = min((cnt[temp - 1] == -1 ? INT32_MAX : cnt[temp - 1]), cnt[temp] + 1);
-            }
-            visited[temp] = true;
-            q.pop();
+        temp = q.front();
+        if(temp < M && cnt[temp * 2] > cnt[temp]) {
+            q.push(temp * 2);
+            cnt[temp * 2] = cnt[temp];
         }
-        if(temp == M) break;
+        if(temp + 1 <= M && cnt[temp + 1] > cnt[temp] + 1) {
+            q.push(temp + 1);
+            cnt[temp + 1] = cnt[temp] + 1;
+        }
+        if(temp - 1 >= 0 && cnt[temp - 1] > cnt[temp] + 1) {
+            q.push(temp - 1);
+            cnt[temp - 1] = cnt[temp] + 1;
+        }
+        q.pop();
     }
     cout << cnt[M];
 }
