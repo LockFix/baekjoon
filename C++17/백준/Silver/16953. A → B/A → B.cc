e8b6d6fbade0ba966c674a69ee57d5821c276ed0
호현 <84440975+LockFix@users.noreply.github.com>
2025-11-14 23:31:14 +0900

    [Silver II] Title: A → B, Time: 24 ms, Memory: 5092 KB -BaekjoonHub

---
+++ C++17/백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int A, B; cin >> A >> B;
+    queue<int> q;
+    int cnt = 0;
+    unordered_set<long long> visited;
+    q.push(A);
+    long long temp = A;
+    while(!q.empty() && temp != B) {
+        int size = q.size();
+        while(size-- && temp != B) {
+            temp = q.front();
+            if(temp * 2 <= B && visited.find(temp * 2) == visited.end()) {
+                visited.insert(temp * 2);
+                q.push(temp * 2);
+            }
+            if(stoll(to_string(temp) + '1') <= B && visited.find(stoll(to_string(temp) + '1')) == visited.end()) {
+                visited.insert(stoll(to_string(temp) + '1'));
+                q.push(stoll(to_string(temp) + '1'));
+            }
+            q.pop();
+        }
+        cnt++;
+    }
+    if(temp == B) cout << cnt;
+    else cout << -1;
+}
