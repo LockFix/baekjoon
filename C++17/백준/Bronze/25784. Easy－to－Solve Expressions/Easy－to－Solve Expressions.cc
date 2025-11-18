bc518765e597e4782e90368e680a6a88685d91c5
호현 <84440975+LockFix@users.noreply.github.com>
2025-11-19 00:02:59 +0900

    [Bronze IV] Title: Easy-to-Solve Expressions, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25784. Easy－to－Solve Expressions/Easy－to－Solve Expressions.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int a, b, c; cin >> a >> b >> c;
+
+    if(a + b == c || b + c == a || a + c == b) cout << 1;
+    else if(a * b == c || b * c == a || a * c == b) cout << 2;
+    else cout << 3;
+}
