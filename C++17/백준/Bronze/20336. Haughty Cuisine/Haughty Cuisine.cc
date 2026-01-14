97b7f905d56f7f64e4f7abee09bc2b906d4bab61
호현 <84440975+LockFix@users.noreply.github.com>
2026-01-14 20:30:05 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    short N; cin >> N;
+
+    short m; cin >> m;
+    cout << m << '\n';
+    while(m--) {
+        string s; cin >> s;
+        cout << s << '\n';
+    }
+}
