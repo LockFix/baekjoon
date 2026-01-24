f97813ebb9b7ba49e6a1249625f14d91d3e0fa9b
호현 <84440975+LockFix@users.noreply.github.com>
2026-01-24 23:19:27 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a, b; cin >> a >> b;
+    short need; cin >> need;
+    int k; cin >> k;
+    
+    cout << (need + 20) * 2;
+}
