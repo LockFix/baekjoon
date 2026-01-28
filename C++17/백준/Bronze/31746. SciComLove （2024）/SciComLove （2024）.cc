90f7e686fa50ea966ac331503bbcb0103d79213b
호현 <84440975+LockFix@users.noreply.github.com>
2026-01-28 22:21:51 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ C++17/백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    short n; cin >> n;
+
+    if(n % 2 == 0) {
+        cout << "SciComLove";
+    } else cout << "evoLmoCicS";
+}
