7dc7be008c5b23f8b94d67b59c441a49e614a7d3
호현 <84440975+LockFix@users.noreply.github.com>
2026-02-23 00:32:30 +0900

    [Bronze IV] Title: Circus, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/20352. Circus/Circus.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long s; cin >> s;
+    fixed(cout);
+    cout.precision(6);
+
+    cout << 2 * 3.1415926 * sqrt(s / 3.1415926);
+}
