8abc283d7deb5e5f7a518055879b218c4592f450
호현 <84440975+LockFix@users.noreply.github.com>
2026-03-13 21:49:59 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.cc
+++ C++17/백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.cc
@@ -10,14 +10,6 @@ int main() {
 
     while(T--) {
         short a, b, c; cin >> a >> b >> c;
-        int result = 0;
-        for(int i = 1; i <= a; i++) {
-            for(int j = 1; j <= b; j++) {
-                for(int k = 1; k <= c; k++) {
-                    if((i % j) == (j % k) && (j % k) == (k % i)) result++;
-                }
-            }
-        }
-        cout << result << '\n';
+        cout << min(a, min(b, c)) << '\n';
     }
 }
